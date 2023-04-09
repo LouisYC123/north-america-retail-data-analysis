c9e1d287175a50d7b088823c9829c0673ec99ff4
LouisYC123 <97873724+LouisYC123@users.noreply.github.com>
2023-04-09 12:39:26 +1000

    added macro to clean column names

--- dbt/dbt_project/models/sources/src_union_all.sql
+++ dbt/dbt_project/models/sources/src_union_all.sql
@@ -1,7 +1,12 @@
 
+
 {% for src in var('raw_data_tables') %}
-    SELECT * FROM {{ source('retail_source', src.name) }}
+    SELECT 
+    {% for col in adapter.get_columns_in_relation(source('retail_source', src.name)) -%}
+        {{ clean_col_names(col.name) }} 
+    {% endfor %}
+       
+    FROM 
+        {{ source('retail_source', src.name) }}
     {{ 'union all' if not loop.last }}
 {% endfor %}
-
-
