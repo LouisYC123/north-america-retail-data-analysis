8e36e4c4b3b07c7eb4c511fe8c0e3f24ffd35bf5
LouisYC123 <97873724+LouisYC123@users.noreply.github.com>
2023-04-10 12:01:59 +1000

    added src_union_raw model

---
+++ dbt/dbt_project/models/sources/src_union_raw.sql
@@ -0,0 +1,27 @@
+{{ config(materialized='view') }}
+
+{% for src in var('raw_data_tables') %}
+    {%- set col_names = adapter.get_columns_in_relation(source('retail_source', src.name)) -%}
+    SELECT
+        REPLACE(REPLACE('{{src.name}}', 'raw_',''), '_data','') AS year
+        {%- for column in col_names -%}
+            {% if column.name != 'TOTAL' %}
+                {% if column.name == 'Unnamed: 0' %}
+                    , "{{ column.name }}"::TEXT AS "naics_code"
+                {% elif column.name == 'Unnamed: 1' %}
+                    , "{{ column.name }}"::TEXT AS "type_of_business"
+                {% elif column.name == 'load_timestamp' %}
+                    , "{{ column.name }}"::TEXT AS "load_timestamp"
+                {% else %}
+                    , "{{ column.name }}"::TEXT AS "{{column.name[:3]}}"
+                {% endif %}
+            {% endif %}
+        {%- endfor %}
+    FROM 
+        {{ source('retail_source', src.name) }}
+    WHERE 
+        "Unnamed: 0" IS NOT NULL
+        AND "Unnamed: 1" IS NOT NULL
+
+    {{ 'union all' if not loop.last }}
+{% endfor %}
