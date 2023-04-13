d592cd23ac86c25a9706fa896a27c9b711e55755
LouisYC123 <97873724+LouisYC123@users.noreply.github.com>
2023-04-13 20:53:25 +1000

    added moving average and book store marts

---
+++ dbt/dbt_project/models/marts/mart_book_stores_sales_by_month.sql
@@ -0,0 +1,13 @@
+WITH source AS (
+    SELECT * FROM {{ ref('stg_unpivot_clean')}}
+)
+
+SELECT
+	date_part('month', sales_month)
+	, round(avg(sales), 2) as avg_sales
+FROM 
+	source
+WHERE 
+	type_of_business = 'Book stores'
+GROUP BY 
+	1
